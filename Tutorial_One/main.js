530cecd0368d9d6141334b981248f3517dfb44d1
Opeyemi <Opeyemi5055@yahoo.com>
2025-06-01 10:10:09 +0100

    webrtc

---
+++ Tutorial_One/main.js
@@ -0,0 +1,53 @@
+// grab out DOM element
+const myVideoContainer = document.getElementById("myVideoContainer");
+const myVideoElement = document.getElementById("myVideo");
+
+let pc = null; // so we can access the pc object in the dev console
+
+myVideoContainer.addEventListener("click", initiateWebRTC)
+
+async function initiateWebRTC(){
+  try {
+    let localStream = await navigator.mediaDevices.getUserMedia({
+      video: true
+    });
+    myVideoElement.srcObject = localStream; // set autoplay in the html
+    // implementing webRTC
+    pc = new RTCPeerConnection();
+    // add the type of media / data we wish to send and receive
+    const tracks = localStream.getTracks();
+    tracks.forEach(track => {
+      pc.addTrack(track);
+      console.log("track was added to pc object");
+    });
+
+    const offer = await pc.createOffer();
+    console.log("offer created", offer);
+
+    // getting stats related to our media track added to out pc
+    let stats = await pc.getStats();
+    stats.forEach(report => {
+      console.log(report);
+    });
+    // add our offer to our pc object
+    pc.setLocalDescription(offer);
+    // listen for ice candidate gathered
+    pc.onicecandidate = (eventObject) => {
+      console.log("ICE RECEIVED:", eventObject.candidate);
+    };
+
+  } catch (error) {
+    console.log(error);
+  };
+}
+
+function getVideoIDs(){
+  navigator.mediaDevices.enumerateDevices()
+    .then(devicesArray => {
+      devicesArray.forEach(devices => {
+        console.log(devices.kind + ": " + devices.label + " id = " + devices.deviceId);
+
+      })
+    })
+  
+}
